ca09175776ebd4d9bb6f1ba05e7795ced838efec
Fang Chi <x25836901@gmail.com>
2021-05-09 14:51:19 +0800

    latest Version - W3

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from flask import Flask, jsonify, render_template, request, abort
 
 mydb = mysql.connector.connect(
     host="127.0.0.1",
-    user="root",
-    password="mydog8229",
+    user="debian-sys-maint",
+    password="exgi5qGqkOVES8BL",
     database="attractions"
 )
 
@@ -148,4 +148,4 @@ def error_500(error):
 
 	
 
-app.run(host="127.0.0.1", port=3000,debug=True)
+app.run(host="0.0.0.0", port=3000,debug=True)
