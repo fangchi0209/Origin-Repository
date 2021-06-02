a7c2d75c04a3ad0262ec7092743d563d5c84e51f
Fang Chi <x25836901@gmail.com>
2021-06-02 21:30:19 +0800

    original file

--- app.py
+++ app.py
@@ -274,175 +274,94 @@ def loginPage():
 @app.route("/api/booking", methods=["GET", "POST", "DELETE"])
 def bookingPage():
 
-    if "memberEmail" in session:
+    try:
+        if "memberEmail" in session:
 
-        if request.method == "POST":
+            if request.method == "POST":
 
-            result = request.get_json()
-            print(result)
-            bookingId = result["attractionId"]
-            date = result["date"]
-            time = result["time2"]
-            price = result["price"]
-            email = session["memberEmail"]
+                result = request.get_json()
+                # print(result)
+                bookingId = result["attractionId"]
+                date = result["date"]
+                time = result["time2"]
+                price = result["price"]
+                email = session["memberEmail"]
+                # print(email)
 
-            if result["date"]:
-                mycursor.execute(
+                if result["date"]:
+                    mycursor.execute(
                     "INSERT INTO booking (booking_date, booking_time, booking_price, booking_id, member_email) VALUES (%s, %s, %s, %s, %s)", (date, time, price, bookingId, email))
-                mydb.commit()
+                    mydb.commit()
 
-                return jsonify({
-                    "ok": True,
-                }), 200
-            else:
-                return jsonify({
-                    "error": True,
-                    "message": "請選取日期"
-                }), 400
+                    return jsonify({
+                        "ok": True,
+                    }), 200
+                else:
+                    return jsonify({
+                        "error": True,
+                        "message": "請選取日期"
+                    }), 400
 
-        if request.method == "GET":
-            bookingEmail = session["memberEmail"]
-            mycursor.execute(
-                "SELECT * FROM booking WHERE member_email = '%s' ORDER BY id DESC LIMIT 1" % (
-                    bookingEmail)
-            )
-            bookingData = mycursor.fetchone()
-            # print(bookingData)
-
-            if bookingData != None:
-                attId = bookingData[4]
+            if request.method == "GET":
+                bookingEmail = session["memberEmail"]
                 mycursor.execute(
-                    "SELECT * FROM information WHERE id = '%s'" % (attId))
-                bookingInfoResult = mycursor.fetchone()
+                    "SELECT * FROM booking WHERE member_email = '%s' ORDER BY id DESC LIMIT 1" % (bookingEmail)
+                )
+                bookingData = mycursor.fetchone()
+                # print(bookingData)
 
+                if bookingData != None:
+                    attId = bookingData[4]
+                    mycursor.execute(
+                        "SELECT * FROM information WHERE id = '%s'" % (attId))
+                    bookingInfoResult = mycursor.fetchone()
+
+                    return jsonify({
+                        "data": {
+                            "attraction": {
+                                "id": bookingInfoResult[0],
+                                "name": bookingInfoResult[1],
+                                "address": bookingInfoResult[4],
+                                "image": bookingInfoResult[9].split(",")[0]
+                            },
+                            "date": bookingData[1],
+                            "time": bookingData[2],
+                            "price": bookingData[3]
+                        }
+                    }), 200
+                else:
+                    return jsonify({
+                        "error": True,
+                        "message": "目前沒有任何待預訂的行程"
+
+                    })
+
+            if request.method == "DELETE":
+                deleteEmail = session["memberEmail"]
+                mycursor.execute(
+                    "DELETE FROM booking WHERE member_email = '%s'" % (
+                        deleteEmail)
+                )
+                mydb.commit()
                 return jsonify({
-                    "data": {
-                        "attraction": {
-                            "id": bookingInfoResult[0],
-                            "name": bookingInfoResult[1],
-                            "address": bookingInfoResult[4],
-                            "image": bookingInfoResult[9].split(",")[0]
-                        },
-                        "date": bookingData[1],
-                        "time": bookingData[2],
-                        "price": bookingData[3]
-                    }
+                    "ok": True
                 }), 200
             else:
                 return jsonify({
-                    "error": True,
-                    "message": "目前沒有任何待預訂的行程"
+                    "error": True
                 })
 
-        if request.method == "DELETE":
-            deleteEmail = session["memberEmail"]
-            mycursor.execute(
-                "DELETE FROM booking WHERE member_email = '%s'" % (
-                    deleteEmail)
-            )
-            mydb.commit()
-            return jsonify({
-                "ok": True
-            }), 200
         else:
             return jsonify({
-                "error": True
-            })
+                "error": True,
+                "message": "請先登入"
+            }), 403
 
-    else:
+    except:
         return jsonify({
             "error": True,
-            "message": "請先登入"
-        }), 403
-
-    # try:
-    #     if "memberEmail" in session:
-
-    #         if request.method == "POST":
-
-    #             result = request.get_json()
-    #             # print(result)
-    #             bookingId = result["attractionId"]
-    #             date = result["date"]
-    #             time = result["time2"]
-    #             price = result["price"]
-    #             email = session["memberEmail"]
-    #             # print(email)
-
-    #             if result["date"]:
-    #                 mycursor.execute(
-    #                 "INSERT INTO booking (booking_date, booking_time, booking_price, booking_id, member_email) VALUES (%s, %s, %s, %s, %s)", (date, time, price, bookingId, email))
-    #                 mydb.commit()
-
-    #                 return jsonify({
-    #                     "ok": True,
-    #                 }), 200
-    #             else:
-    #                 return jsonify({
-    #                     "error": True,
-    #                     "message": "請選取日期"
-    #                 }), 400
-
-    #         if request.method == "GET":
-    #             bookingEmail = session["memberEmail"]
-    #             mycursor.execute(
-    #                 "SELECT * FROM booking WHERE member_email = '%s' ORDER BY id DESC LIMIT 1" % (bookingEmail)
-    #             )
-    #             bookingData = mycursor.fetchone()
-    #             # print(bookingData)
-
-    #             if bookingData != None:
-    #                 attId = bookingData[4]
-    #                 mycursor.execute(
-    #                     "SELECT * FROM information WHERE id = '%s'" % (attId))
-    #                 bookingInfoResult = mycursor.fetchone()
-
-    #                 return jsonify({
-    #                     "data": {
-    #                         "attraction": {
-    #                             "id": bookingInfoResult[0],
-    #                             "name": bookingInfoResult[1],
-    #                             "address": bookingInfoResult[4],
-    #                             "image": bookingInfoResult[9].split(",")[0]
-    #                         },
-    #                         "date": bookingData[1],
-    #                         "time": bookingData[2],
-    #                         "price": bookingData[3]
-    #                     }
-    #                 }), 200
-    #             else:
-    #                 return jsonify({
-    #                     "error": True,
-    #                     "message": "目前沒有任何待預訂的行程"
-
-    #                 })
-
-    #         if request.method == "DELETE":
-    #             deleteEmail = session["memberEmail"]
-    #             mycursor.execute(
-    #                 "DELETE FROM booking WHERE member_email = '%s'" % (
-    #                     deleteEmail)
-    #             )
-    #             mydb.commit()
-    #             return jsonify({
-    #                 "ok": True
-    #             }), 200
-    #         else:
-    #             return jsonify({
-    #                 "error": True
-    #             })
-
-    #     else:
-    #         return jsonify({
-    #             "error": True,
-    #             "message": "請先登入"
-    #         }), 403
-
-    # except:
-    #     return jsonify({
-    #         "error": True,
-    #         "message": "伺服器內部錯誤"
-    #     }), 500
+            "message": "伺服器內部錯誤"
+        }), 500
 
 
 @app.route("/api/orders", methods=["POST"])
