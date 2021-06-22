d00424c0eb8544541db6c1266aefda6b70d7d785
Fang Chi <x25836901@gmail.com>
2021-06-22 16:44:41 +0800

    re-add dotenv_path

--- module/connectMysql.py
+++ module/connectMysql.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import mysql.connector
 from mysql.connector import pooling, Error
 
-load_dotenv()
+load_dotenv(dotenv_path="/home/ubuntu/Origin_Respository/.env")
 
 connection_pool = pooling.MySQLConnectionPool(
     pool_name = os.getenv("DBpool"),
